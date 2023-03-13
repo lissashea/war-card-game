bc9bf0010be2581bde6ce5e9245838c6a00149a2
“Lissa <lwarshaw674@gmail.com>
2023-03-13 15:41:12 -0400

    instances added

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // In your main file where you want to start the game
 const GameOfWar = require('./GameOfWar');
 
-const game1 = new GameOfWar();
+const game2 = new GameOfWar();
+const game3 = new GameOfWar();
+const game4 = new GameOfWar();
+const game5 = new GameOfWar();
